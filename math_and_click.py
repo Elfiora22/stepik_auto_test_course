69505da6d2ac6d406b153c6f120c72a06b323f71
Lena <elfiora2@icloud.com>
2023-02-01 22:36:28 -0800

    Add files via upload

---
+++ math_and_click.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+
+try:
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    import math
+    def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+    
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+
+    
+    y_input = browser.find_element(By.ID, 'answer')
+    y_input.send_keys(y)
+    
+    robot_check = browser.find_element(By.ID, 'robotCheckbox')
+    robot_check.click()
+    robot_rule = browser.find_element(By.ID, 'robotsRule')
+    robot_rule.click()
+    button = browser.find_element(By.CSS_SELECTOR, '.btn.btn-default')
+    button.click()
+    
+finally:
+    time.sleep(30)
+    browser.quit()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
